d7cb23262d79e77922f3450623df8e560ef1fd02
Nisharg Soni <ncsoni9898@gmail.com>
2024-07-03 09:04:53 +0530

    Added Auth Middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("./catchAsyncError");
+const User = require("../models/userModal");
+const jwt = require("jsonwebtoken");
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("Please Login To Access This User", 401));
+  }
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decodedData.id);
+  next();
+});
+exports.authorizedRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
