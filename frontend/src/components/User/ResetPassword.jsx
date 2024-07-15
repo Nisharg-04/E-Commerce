d065400f364b2c78dad5a541071dd6de6a2d52ee
Nisharg Soni <ncsoni9898@gmail.com>
2024-07-15 07:50:42 +0530

    Added Components of Update and Forgot Password

---
+++ frontend/src/components/User/ResetPassword.jsx
@@ -0,0 +1,92 @@
+import React, {useState, useEffect } from "react";
+import "./ResetPassword.css";
+import Loader from "../layout/Loader/Loader";
+import { useDispatch, useSelector } from "react-redux";
+import MetaData from "../layout/MetaData";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import LockIcon from "@material-ui/icons/Lock";
+import { useNavigate, useParams } from "react-router-dom";
+import { resetPassword } from "../../Slices/UserSlices/ForgotPasswordSlice";
+
+const ResetPassword = () => {
+    const {token}=useParams();
+    let navigate=useNavigate();
+  const dispatch = useDispatch();
+
+  const { error, success, loading } = useSelector(
+    (state) => state.forgotPassword
+  );
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const resetPasswordSubmit = (e) => {
+    e.preventDefault();
+
+    const myForm = new FormData();
+
+    myForm.set("password", password);
+    myForm.set("confirmPassword", confirmPassword);
+
+    dispatch(resetPassword({token,password,confirmPassword}));
+  };
+
+  useEffect(() => {
+
+    if (success) {
+  {    alert("Password Updated Successfully");}
+
+     navigate(`/login`);
+    }
+  }, [dispatch, success]);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MetaData title="Change Password" />
+          <div className="resetPasswordContainer">
+            <div className="resetPasswordBox">
+              <h2 className="resetPasswordHeading">Update Profile</h2>
+
+              <form
+                className="resetPasswordForm"
+                onSubmit={resetPasswordSubmit}
+              >
+                <div>
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="New Password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="loginPassword">
+                  <LockIcon />
+                  <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    required
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Update"
+                  className="resetPasswordBtn"
+                />
+              </form>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default ResetPassword;
