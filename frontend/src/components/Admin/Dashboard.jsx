77b30ad0d1db286c94ae1eb407008fd32803d4e7
Nisharg Soni <ncsoni9898@gmail.com>
2024-07-29 09:09:26 +0530

    Added Frontend For Admin Dashboard

---
+++ frontend/src/components/Admin/Dashboard.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import Sidebar from "./Sidebar.jsx";
+import "./dashboard.css";
+import { Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { Doughnut, Line } from "react-chartjs-2";
+import { useSelector, useDispatch } from "react-redux";
+import MetaData from "../layout/MetaData";
+import { getAdminProducts } from "../../Slices/ProductSlices/AdminProductsSlice.js";
+import { getAllOrders } from "../../Slices/OrderSlices/GetOrderSlice";
+import { getAllUsers } from "../../Slices/UserSlices/GetAllUsersSlice.js";
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  const { products } = useSelector((state) => state.products);
+
+  const { orders } = useSelector((state) => state.allOrders);
+
+  const { users } = useSelector((state) => state.allUsers);
+
+  let outOfStock = 0;
+
+  products &&
+    products.forEach((item) => {
+      if (item.Stock === 0) {
+        outOfStock += 1;
+      }
+    });
+
+  useEffect(() => {
+    dispatch(getAdminProducts());
+
+    dispatch(getAllOrders());
+    dispatch(getAllUsers());
+  }, [dispatch]);
+
+  let totalAmount = 0;
+  orders &&
+    orders.forEach((item) => {
+      totalAmount += item.totalPrice;
+    });
+  return (
+    <div className="dashboard">
+      <MetaData title="Dashboard - Admin Panel" />
+      <Sidebar />
+
+      <div className="dashboardContainer">
+        <Typography component="h1">Dashboard</Typography>
+
+        <div className="dashboardSummary">
+          <div>
+            <p>
+              Total Amount <br /> ₹{totalAmount}
+            </p>
+          </div>
+          <div className="dashboardSummaryBox2">
+            <Link to="/admin/products">
+              <p>Product</p>
+              <p>{products && products.length}</p>
+            </Link>
+            <Link to="/admin/orders">
+              <p>Orders</p>
+              <p>{orders && orders.length}</p>
+            </Link>
+            <Link to="/admin/users">
+              <p>Users</p>
+              <p>{users && users.length}</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
